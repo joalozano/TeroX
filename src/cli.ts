ae9c5178c5404e80797b892c68b794f2ac1f7932
cozin101 <joalozano@proton.me>
2025-08-29 15:51:04 -0300

    Terminando de copiar el trabajo hecho por la cátedra. Recordar cambiar todo una vez tengamos que implementar nuestro proyecto

--- src/cli.ts
+++ src/cli.ts
@@ -1,2 +1,65 @@
 import { Client } from 'pg';
 import { readFile } from 'node:fs/promises';
+
+async function leerYParsearCsv(filePath)
+{
+    const contents = await readFile(filePath, { encoding: 'utf8' });
+    const header = contents.split(/\r?\n/)[0];
+    const columns = header.split(',').map(col => col.trim());
+    const dataLines = contents.split(/\r?\n/).slice(1).filter(line => line.trim() !== '');
+    return {dataLines, columns};
+}
+
+async function refrescarTablaAlumnos(clientDb, listaDeAlumnosCompleta, columnas)
+{
+    await clientDb.query("DELETE FROM aida.alumnos");
+    for (const line of listaDeAlumnosCompleta) {
+        const values = line.split(',');
+        const query = `
+            INSERT INTO aida.alumnos (${columnas.join(', ')}) VALUES
+                (${values.map((value) => value == '' ? 'null' : `'` + value + `'`).join(', ')})
+        `;
+        console.log(query);
+        const res = await clientDb.query(query);
+        console.log(res.command, res.rowCount);
+    }
+}
+
+async function obtenerPrimerAlumnoQueNecesitaCertificado(clientDb)
+{
+    const sql = `SELECT *
+    FROM aida.alumnos
+    WHERE titulo IS NOT NULL AND titulo_en_tramite IS NOT NULL
+    ORDER BY egreso
+	LIMIT 1`;
+    const res = await clientDb.query(sql);
+    if (res.rows.length > 0) {
+        return res.rows[0];
+    } else {
+        return null;
+    }
+}
+
+
+async function generarCertificadoParaAlumno(clientDb, alumno)
+{
+    console.log('alumno', alumno);
+}
+
+async function principal()
+{
+    const clientDb = new Client();
+    const filePath = `recursos/alumnos.csv`;
+    await clientDb.connect();
+    var {dataLines: listaDeAlumnosCompleta, columns: columnas} = await leerYParsearCsv(filePath)
+    await refrescarTablaAlumnos(clientDb, listaDeAlumnosCompleta, columnas);
+    var alumno = await obtenerPrimerAlumnoQueNecesitaCertificado(clientDb);
+    if (alumno == null) {
+        console.log('No hay alumnos que necesiten certificado');
+    } else {
+        await generarCertificadoParaAlumno(clientDb, alumno);
+    }
+    await clientDb.end();
+}
+
+principal();
