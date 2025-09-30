0226ceb47b1309c957cd2cda9fc8702fa5d5ac22
Mario <mlivia@dc.uba.ar>
2025-09-29 21:21:04 -0300

    conexión exitosa a la app. Y seteo de variables de ambiente para la base de datos

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import rutas_de_productos from "./routes/productos";
+import pool from "./config/db.ts";
 
 const app = express();
 
 app.use(express.json());
+app.set('view engine', 'ejs')
+
 app.use("/productos", rutas_de_productos);
 
 app.listen(3000, () => {
-	console.log("Servidor iniciado en el puerto 3000");
+	console.log("Servidor iniciado en http://localhost:3000");
 });
+
