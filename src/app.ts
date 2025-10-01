99220a6e81340f3756851d80c2b87e00bf414d47
Nacho <lucasignaciorodriguez123compu@gmail.com>
2025-10-01 16:31:52 -0300

    Primer endpoint en funcionamiento

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import rutas_de_productos from "./routes/productos";
-import pool from "./config/db.ts";
+//import pool from "./config/db";
 
 const app = express();
 
@@ -8,6 +8,10 @@ app.use(express.json());
 app.set('view engine', 'ejs')
 
 app.use("/productos", rutas_de_productos);
+
+app.get("/", async (_, res) => {
+	res.render('index');
+});
 
 app.listen(3000, () => {
 	console.log("Servidor iniciado en http://localhost:3000");
