ad534a30febd275dd1c09bf2b6d7467390d8f31c
Nacho <lucasignaciorodriguez123compu@gmail.com>
2025-10-01 15:46:26 -0300

    Primera version de la tabla productos

--- recursos/creacion-schema.sql
+++ recursos/creacion-schema.sql
@@ -3,10 +3,13 @@ create schema terox;
 grant usage on schema terox to terox_admin;
 
 -- Tabla de Productos
-create table terox.productos
--- Implementar
--- atributos posibles: nombre, precio, stock, descripcion, fecha_creacion, imagen_url
--- cual podría ser una buena primary key?
+create table terox.productos (
+    producto_id serial primary key,
+    nombre varchar(100) not null,
+    precio int not null check (precio >= 0),
+    stock int not null check (stock >= 0),
+    descripcion text,
+    imagen_url varchar(255)
 );
 
 grant select, insert, update, delete on terox.productos to terox_admin;
