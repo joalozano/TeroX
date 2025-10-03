65d34f3e789f367192001c8753025c6cf1c35425
Mario <mlivia@dc.uba.ar>
2025-10-03 16:58:35 -0300

    Funcionalidad dinámica de borrado implementada. Scripts más conexión al backend. Con respuestas y validaciones en el front y back básicas. Falta en el front responder adecuadamente si el producto no puede ser borrado. Resolución de problemas con las rutas para las imagenes y el script.

--- src/routes/productos.ts
+++ src/routes/productos.ts
@@ -3,10 +3,65 @@ import pool from "../config/db";
 
 const router = Router();
 
-router.get("/", async (_, res) => {
+router.get("/ver/productos", async (_, res) => {
 	const productos = await pool.query('SELECT * FROM terox.productos');
 
 	return res.render('productos', { productos: productos.rows });
+});
+
+router.post("/agregar/productos/", async (req, res) => {
+	const nombre_producto = req.body.nombre_del_producto;
+	const precio = parseInt(req.body.precio_del_producto);
+	const stock = parseInt(req.body.stock_del_producto);
+	const descripcion = req.body.descripcion_del_producto;
+	// const imagen, guardamos la imagen
+	const imagen_url = null;
+
+	try {
+		await pool.query(`INSERT INTO terox.productos 
+			(nombre, precio, stock, descripcion, imagen_url) VALUES ($1, $2, $3, $4, $5)`,
+			[nombre_producto, precio, stock, descripcion, imagen_url]);
+		return res.sendStatus(200);
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error("Error al eliminar producto:", error.message);
+		} else {
+			console.error("Error desconocido:", error);
+		}
+
+		return res.sendStatus(400);
+	}
+
+})
+
+router.post("/editar/productos/", async (_, res) => {
+	//const producto_id = parseInt(req.body.producto_id);
+
+	return res.sendStatus(404);
+});
+
+router.post("/borrar/productos/", async (req, res) => {
+	console.log(req.body);
+	const producto_id = parseInt(req.body.producto_id);
+
+	// esto la bd lo valida, pero capaz no me indica qué fue esto lo que falló
+	if (!Number.isInteger(producto_id) || producto_id <= 0) {
+		return res.status(400).json({ error: 'producto_id inválido' });
+	}
+
+	try {
+		await pool.query(`DELETE FROM terox.productos WHERE producto_id = $1`, [producto_id]);
+		console.log("BORRAMOS PRODUCTO CON ID:", producto_id);
+		return res.status(200).json({ producto_borrado: producto_id });
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error("Error al eliminar producto:", error.message);
+		} else {
+			console.error("Error desconocido:", error);
+		}
+
+		return res.sendStatus(400);
+	}
 });
 
 export default router;
