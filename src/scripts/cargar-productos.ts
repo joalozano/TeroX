35df2cb4ee7ea3e14099eaa27c25439ea819d1ae
Nacho <lucasignaciorodriguez123compu@gmail.com>
2025-10-07 14:30:03 -0300

    arreglando conflictos al separar los endpoints del crud y views

---
+++ src/scripts/cargar-productos.ts
@@ -0,0 +1,44 @@
+async function cargarProductos() {
+    const lista = document.getElementById("lista_productos")!;
+    const mensajeEstado = document.getElementById("mensaje_estado")!;
+
+    try {
+        mensajeEstado.textContent = "Cargando productos...";
+
+        const respuesta = await fetch("/api/ver/productos", {
+            method: "GET"
+        });
+
+        if (!respuesta.ok) throw new Error("Error al obtener productos");
+
+        const productos = await respuesta.json();
+
+        if (!productos || productos.length === 0) {
+            mensajeEstado.textContent = "No hay productos disponibles :(";
+            return;
+        }
+
+        mensajeEstado.remove()
+
+        for (const producto of productos) {
+            const li = document.createElement("li");
+            li.id = producto.producto_id;
+            li.className = "card_product";
+            li.innerHTML = `
+				<img src="${producto.imagen_url}" alt="${producto.nombre}" width="200" />
+				<p>Precio: $${producto.precio}</p>
+				<p>Descripción: ${producto.descripcion}</p>
+				<form class="pedido_de_borrado" data-id="${producto.producto_id}">
+					<button type="submit">Borrar Producto</button>
+				</form>
+				<br>
+			`;
+            lista.appendChild(li);
+        }
+    } catch (error) {
+        console.error(error);
+        mensajeEstado.textContent = "Ocurrió un error al cargar los productos.";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", cargarProductos);
