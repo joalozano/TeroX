7684a0cfbbe6a6c9fd0fabfef3b6fbd7c506b491
Mario <mlivia@dc.uba.ar>
2025-10-08 18:26:15 -0300

    script para añadir usuario de test y interfaz para poder trabajar crear, autenticar y cambiar contraseña a un usuario

--- recursos/cargar-datos-de-test.sql
+++ recursos/cargar-datos-de-test.sql
@@ -6,3 +6,13 @@ INSERT INTO terox.productos (
     imagen_url)
     VALUES ('Big Dipper', '20', '1', 'Goma Pegajosa de Tenis de Mesa, con tecnología MaxTense', 'big_dipper.avif')
 
+INSERT INTO terox.usuarios (
+    username,
+    password_hash,
+    email,
+    nombre
+)
+VALUES ('messi777', '$2b$10$v1xa5hA7RdBGwSJgDVIl6eZ49SJSTRt0n4xzDqhovDjJeISk/OyOC', 'pepe@gmail.com', 'Pepe Van Houten');
+
+/*la contraseña de messi777 es messi*/
+
