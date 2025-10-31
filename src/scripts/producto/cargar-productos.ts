f62c0c450d9b4ff3f1d84d8322f8221431a55abc
Mario <mlivia@dc.uba.ar>
2025-10-31 13:00:46 -0300

    cambios menores

--- src/scripts/producto/cargar-productos.ts
+++ src/scripts/producto/cargar-productos.ts
@@ -1,5 +1,7 @@
+import { setAttrs } from "../html-operation/html_elements.js";
+
 async function cargarProductos(url: string) {
-    const lista = document.getElementById("lista_productos")!;
+    const lista: HTMLElement = document.getElementById("lista_productos")!;
     const mensajeEstado = document.getElementById("mensaje_estado")!;
 
     try {
@@ -21,19 +23,7 @@ async function cargarProductos(url: string) {
         mensajeEstado.remove()
 
         for (const producto of productos) {
-            const li = document.createElement("li");
-            li.id = producto.producto_id;
-            li.className = "card-product";
-            li.innerHTML = `
-				<img src="/uploads/${producto.producto_id}" alt="${producto.nombre}" width="200" />
-				<h3>${producto.nombre}</h3>
-                                <p>Precio: $${producto.precio}</p>
-				<p>Descripción: ${producto.descripcion}</p>
-                                <p>Stock: ${producto.stock}</p>
-				<button  class="pedido_de_borrado" data-id="${producto.producto_id}">Borrar Producto</button>
-				<button class="pedido_de_edicion" data-id="${producto.producto_id}">Editar Producto</button>
-			`;
-            lista.appendChild(li);
+            crear_contenedor_producto_y_agregar_a_lista(producto, lista);
         }
     } catch (error) {
         console.error(error);
@@ -41,4 +31,61 @@ async function cargarProductos(url: string) {
     }
 }
 
-export { cargarProductos };
+function crear_contenedor_producto_y_agregar_a_lista(producto: any, lista_productos: HTMLElement) {
+    const contenedor_producto = 
+                crear_elemento_con_nombre_attrs_y_textcontent(
+                'li', { id: producto.producto_id as string, class: 'card-product', }, '');
+            
+            const imagen = 
+                crear_elemento_con_nombre_attrs_y_textcontent(
+                'img', {src : `/uploads/${producto.producto_id}`, 
+                alt : producto.nombre, width : '200'}, '');
+            contenedor_producto.appendChild(imagen);
+
+            const nombre = 
+                crear_elemento_con_nombre_attrs_y_textcontent(
+                'h3', {}, `${producto.nombre}`)
+            contenedor_producto.appendChild(nombre);
+
+            const precio = 
+                crear_elemento_con_nombre_attrs_y_textcontent(
+                'p', {}, `Precio: $${producto.precio}`)
+            contenedor_producto.appendChild(precio)
+            
+            const descripcion = 
+                crear_elemento_con_nombre_attrs_y_textcontent(
+                'p', {}, 'Descripción: ' + producto.descripcion);
+            contenedor_producto.appendChild(descripcion);
+
+            const stock = 
+                crear_elemento_con_nombre_attrs_y_textcontent(
+                'p', {}, `Stock: ${producto.stock}`);
+            contenedor_producto.appendChild(stock);
+
+            const pedido_de_borrado = 
+                crear_elemento_con_nombre_attrs_y_textcontent(
+                'button', { class: 'pedido_de_borrado', 'data-id': producto.producto_id as string }, 'Borrar Producto');
+            contenedor_producto.appendChild(pedido_de_borrado);
+
+            const pedido_de_edicion = 
+                crear_elemento_con_nombre_attrs_y_textcontent(
+                'button', { class: 'pedido_de_edicion', 'data-id': producto.producto_id as string }, 'Editar Producto');
+            contenedor_producto.appendChild(pedido_de_edicion);
+
+            lista_productos.appendChild(contenedor_producto);
+}
+
+function crear_elemento_con_nombre_attrs_y_textcontent
+(etiqueta: string, attrs: {[key: string]: string}, textContent: string) {
+    
+    const elemento = document.createElement(etiqueta);
+    setAttrs(elemento, attrs);
+    elemento.textContent = textContent
+    return elemento;
+}
+
+
+
+export { cargarProductos, crear_contenedor_producto_y_agregar_a_lista as crear_contenedor_producto };
+
+
