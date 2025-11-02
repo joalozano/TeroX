8c2c07744602f0b8c7eb88b3bfc982978fdcc331
Nacho <lucasignaciorodriguez123compu@gmail.com>
2025-11-02 00:33:32 -0300

    refactor de app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,72 +1,21 @@
 import express from "express";
-import generarCRUD from "./routes/crud-api";
-import productos_views from "./routes/productos-views";
-
-import session from 'express-session';
-
+import routes from "./routes/index";
+import path from "path";
+import session from "express-session";
+import sessionConfig from "./config/session";
 import errorHandler from "./middlewares/middlewares-error-handler";
-import { requireAuthAPI, replacePasswordForHash } from './middlewares/middlewares-auth'
-import { añadir_usuario_id_a_request, verificar_usuario_es_dueño_del_producto } from './middlewares/middlewares-productos'
-import auth_api from "./routes/auth-api";
-import user_session_views from "./routes/user-sesion-views";
-import imagenes_routes from "./routes/images-routes";
-import productos_routes from "./routes/productos";
-
-declare module 'express-session' {
-    interface SessionData {
-        usuario?: Usuario;
-    }
-}
-
 
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(session(sessionConfig));
 
-app.use(session({
-    secret: process.env["SESSION_SECRET"] || "para que tipe",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: false,
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 // 1 día
-    }
-}));
-
-const atributos_producto = ["nombre", "descripcion", "precio", "stock", "usuario_id"];
-const middlewares_producto: MiddlewareCRUD = {
-    get: [],
-    post: [requireAuthAPI, añadir_usuario_id_a_request],
-    put: [requireAuthAPI, añadir_usuario_id_a_request, verificar_usuario_es_dueño_del_producto],
-    delete: [requireAuthAPI, verificar_usuario_es_dueño_del_producto]
-};
-
-app.use("/api", generarCRUD("/productos", "producto_id", atributos_producto, middlewares_producto));
-app.use("/api", productos_routes);
-
-const atributos_usuario = ["username", "password_hash", "nombre", "email"];
-const middlewares_usuarios: MiddlewareCRUD = {
-    get: [(_, res, __) => { res.sendStatus(403); }],
-    post: [replacePasswordForHash],
-    put: [],
-    delete: []
-};
-
-app.use("/api", generarCRUD("/usuarios", "usuario_id", atributos_usuario, middlewares_usuarios));
-app.use("/api/auth", auth_api);
-
-// Manejo de errores
+app.use(routes);
 app.use(errorHandler);
-
-
-app.use("/", productos_views);
-app.use("/", user_session_views);
-app.use("/", imagenes_routes);
-
 
 app.get("/", async (_, res) => {
     res.render('index');
