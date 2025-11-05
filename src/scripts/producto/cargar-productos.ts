2a6fe9cc0447013acedac4b00cadbfb8690d3f53
cozin101 <Estudiante@pc16.lab1104.labos.fcen.uba.ar>
2025-11-05 16:18:37 -0300

    Comentando botones de edición/borrado, agregando botón dummy de compra

--- src/scripts/producto/cargar-productos.ts
+++ src/scripts/producto/cargar-productos.ts
@@ -62,7 +62,7 @@ function crear_contenedor_producto_y_agregar_a_lista(producto: any, lista_produc
                 'p', {}, `Stock: ${producto.stock}`);
             contenedor_producto.appendChild(stock);
 
-            const pedido_de_borrado = 
+            /*const pedido_de_borrado = 
                 crear_elemento_con_nombre_attrs_y_textcontent(
                 'button', { class: 'pedido_de_borrado', 'data-id': producto.producto_id as string }, 'Borrar Producto');
             contenedor_producto.appendChild(pedido_de_borrado);
@@ -70,7 +70,11 @@ function crear_contenedor_producto_y_agregar_a_lista(producto: any, lista_produc
             const pedido_de_edicion = 
                 crear_elemento_con_nombre_attrs_y_textcontent(
                 'button', { class: 'pedido_de_edicion', 'data-id': producto.producto_id as string }, 'Editar Producto');
-            contenedor_producto.appendChild(pedido_de_edicion);
+            contenedor_producto.appendChild(pedido_de_edicion);*/
+	   const boton_compra=
+		   crear_elemento_con_nombre_attrs_y_textcontent(
+		   'button', { class: 'boton_compra', 'data-id': producto.producto_id as string}, 'Comprar');
+	   contenedor_producto.appendChild(boton_compra);
 
             lista_productos.appendChild(contenedor_producto);
 }
