18f400f32691cd18a15a6cea759de1b1ca8dbfd9
cozin101 <joalozano@proton.me>
2025-11-16 17:58:47 -0300

    Agregando posibles atributos/tablas para implementar facturas; cambiando tabulación

--- recursos/creacion-schema.sql
+++ recursos/creacion-schema.sql
@@ -4,30 +4,46 @@ GRANT usage ON schema terox TO terox_admin;
 
 -- Tabla de Usuarios
 CREATE TABLE IF NOT EXISTS terox.usuarios (
-    username TEXT PRIMARY KEY,
-    password_hash TEXT NOT NULL,
-    nombre TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL
+	username TEXT PRIMARY KEY,
+	password_hash TEXT NOT NULL,
+	nombre TEXT NOT NULL,
+	email TEXT UNIQUE NOT NULL
 );
 
 -- Tabla de Productos
 CREATE TABLE IF NOT EXISTS terox.productos (
-    producto_id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    precio INT NOT NULL CHECK (precio >= 0),
-    stock INT NOT NULL CHECK (stock >= 0),
-    descripcion TEXT,
-    username TEXT NOT NULL REFERENCES terox.usuarios(username) ON DELETE CASCADE
+	producto_id SERIAL PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	precio INT NOT NULL CHECK (precio >= 0),
+	stock INT NOT NULL CHECK (stock >= 0),
+	descripcion TEXT,
+	username TEXT NOT NULL REFERENCES terox.usuarios(username) ON DELETE CASCADE
 );
 
 
 -- Tabla de Imágenes
 -- url es relativa a alguna carpeta base de imágenes
 CREATE TABLE IF NOT EXISTS terox.imagenes(
-    imagen_id SERIAL PRIMARY KEY,
-    producto_id INT REFERENCES terox.productos(producto_id) ON DELETE CASCADE,
-    url TEXT NOT NULL
+	imagen_id SERIAL PRIMARY KEY,
+	producto_id INT REFERENCES terox.productos(producto_id) ON DELETE CASCADE,
+	url TEXT NOT NULL
 );
+
+--CREATE TABLE IF NOT EXISTS terox.pedidos/facturas(
+--	-rating: int entre 0 y 5, podría ser -1 (si no se vota)
+--	el rating se usa para calcular AVG al mostrar un usuario en la interfaz, si es que se desea implementar
+--	esto. La alternativa es agregar RATING a usuario, y cada vez que se realiza una "votación" de ranking,
+--	se actualiza el puntaje del usuario (mucho mejor que recorrer todos los pedidos calculando un avg).
+--	-cantidad, precio unitario al momento de la compra: ints. Alternativamente, guardar el precio pagado y la cantidad comprada
+--	Keys:
+--	-username comprador, fecha, producto_id primary key???
+--	alternativamente:
+--	-pedido_id: primary key
+--	-fecha
+--	-username comprador: foreign key
+--	-producto_id: foreign key
+--	-username vendedor foreign key???, podría facilitar el cálculo del AVG
+--);
 
 -- Permisos
 
