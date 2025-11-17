a12a680b9c7e5501543cff22452be8ea5d0849ef
Nacho <lucasignaciorodriguez123compu@gmail.com>
2025-11-16 21:57:32 -0300

    Refactor al crear los productos

--- src/scripts/html-operation/producto-html.ts
+++ src/scripts/html-operation/producto-html.ts
@@ -1,9 +1,8 @@
+import { productoTableDef } from "../estructuras.js";
 import { setAttrs } from "../html-operation/html_elements.js";
+import { crearElementoDesdeRegistro } from "./crear-lista.js";
 
-export function crear_contenedor_producto_y_agregar_a_lista(producto: any, lista_productos: HTMLElement) {
-    const contenedor_producto =
-        crear_elemento_con_nombre_attrs_y_textcontent(
-            'li', { id: producto.producto_id as string, class: 'card-product', }, '');
+export function agregarProductoALista(producto: any, lista: HTMLElement) {
 
     const imagen =
         crear_elemento_con_nombre_attrs_y_textcontent(
@@ -11,41 +10,27 @@ export function crear_contenedor_producto_y_agregar_a_lista(producto: any, lista
             src: `/uploads/${producto.producto_id}`,
             alt: producto.nombre, width: '200'
         }, '');
-    contenedor_producto.appendChild(imagen);
 
-    const nombre =
-        crear_elemento_con_nombre_attrs_y_textcontent(
-            'h3', {}, `${producto.nombre}`)
-    contenedor_producto.appendChild(nombre);
+    const botonBorrar = crear_elemento_con_nombre_attrs_y_textcontent(
+        "button",
+        { class: "pedido_de_borrado", "data-id": producto.producto_id },
+        "Borrar"
+    );
 
-    const precio =
-        crear_elemento_con_nombre_attrs_y_textcontent(
-            'p', {}, `Precio: $${producto.precio}`)
-    contenedor_producto.appendChild(precio)
+    const botonEditar = crear_elemento_con_nombre_attrs_y_textcontent(
+        "button",
+        { class: "pedido_de_edicion", "data-id": producto.producto_id },
+        "Editar"
+    );
 
-    const descripcion =
-        crear_elemento_con_nombre_attrs_y_textcontent(
-            'p', {}, 'Descripción: ' + producto.descripcion);
-    contenedor_producto.appendChild(descripcion);
+    const item = crearElementoDesdeRegistro(productoTableDef, producto, imagen, [
+        botonBorrar,
+        botonEditar
+    ]);
 
-    const stock =
-        crear_elemento_con_nombre_attrs_y_textcontent(
-            'p', {}, `Stock: ${producto.stock}`);
-    contenedor_producto.appendChild(stock);
-
-    const pedido_de_borrado = 
-        crear_elemento_con_nombre_attrs_y_textcontent(
-        'button', { class: 'pedido_de_borrado', 'data-id': producto.producto_id as string }, 'Borrar Producto');
-    contenedor_producto.appendChild(pedido_de_borrado);
-
-    const pedido_de_edicion = 
-        crear_elemento_con_nombre_attrs_y_textcontent(
-        'button', { class: 'pedido_de_edicion', 'data-id': producto.producto_id as string }, 'Editar Producto');
-    contenedor_producto.appendChild(pedido_de_edicion);
-
-
-    lista_productos.appendChild(contenedor_producto);
+    lista.appendChild(item);
 }
+
 
 export function crear_elemento_con_nombre_attrs_y_textcontent
     (etiqueta: string, attrs: { [key: string]: string }, textContent: string) {
