9233d1c4258ff8ceea27e46758a87193da1b5a7b
joalozano <147951962+joalozano@users.noreply.github.com>
2025-11-19 18:42:02 -0300

    Fix ruta get.js en comprar.ts

--- src/scripts/comprar.ts
+++ src/scripts/comprar.ts
@@ -1,4 +1,4 @@
-import { getElementByID } from "../html-operation/get.js";
+import { getElementByID } from "../utils/get-elements-by-util.js";
 
 async function comprar_productos(url: string) {
     // Usar event delegation en el contenedor de todos los productos
