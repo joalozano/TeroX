2d388c5467611ecf17b426aa2ca633dcfeec29ea
joalozano <joaquin.lozano.trabajo@gmail.com>
2025-11-25 17:34:20 -0300

    Eliminando funciones de procesamiento de factura: el modelo cambió para que todas las órdenes se consideren pagas

--- src/config/listener.ts
+++ src/config/listener.ts
@@ -12,23 +12,8 @@ export async function initListener() {
       if (!link) console.error("[ERROR] Payload vacío en imagen_borrada");
       await borrarImagen(link as string);
     }
-	if (msg.channel === "orden_aceptada") {
-	  const orden_id: number = Number(msg.payload);
-	  await crearFactura(orden_id);
-	}
   });
 
   await listener.query("LISTEN imagen_borrada");
   console.log("Listener Activo, esperando eventos...");
-}
-
-async function crearFactura(orden_id: number) {
-	const query_ObtenerUsernames = `SELECT comprador_username, vendedor_username FROM terox.ordenes WHERE orden_id = $1`;
-	const query_ObtenerCUIL = `SELECT cuil FROM terox.identidad_fiscal WHERE username = $1`;
-	const query_crearFactura = `INSERT INTO terox.facturas VALUES($1, $2, $3)`;
-	const usernames = await executeQuery(query_ObtenerUsernames, [orden_id], '');
-	const cuil_comprador = await executeQuery(query_ObtenerCUIL, [usernames.rows[0]?.comprador_username],'');
-	const cuil_vendedor = await executeQuery(query_ObtenerCUIL, [usernames.rows[0]?.vendedor_username],'');
-	await executeQuery(query_crearFactura, [orden_id, cuil_comprador, cuil_vendedor], 'Error al crear factura');
-	return;
 }
