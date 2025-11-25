6b80ff0c61339c96b6e9c809c8c1ea116ad9bf9f
joalozano <joaquin.lozano.trabajo@gmail.com>
2025-11-25 17:03:20 -0300

    Cambiando scripts para no usar rankings y avisar al usuario que verifique el script antes de correr

--- recursos/dar-permisos.sql
+++ recursos/dar-permisos.sql
@@ -12,5 +12,3 @@ GRANT USAGE, SELECT, UPDATE ON SEQUENCE terox.ordenes_orden_id_seq TO terox_admi
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON terox.facturas TO terox_admin;
 GRANT USAGE, SELECT, UPDATE ON SEQUENCE terox.facturas_factura_id_seq TO terox_admin;
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON terox.ratings TO terox_admin;
--- recursos/inicializar-postgreSQL.sh
+++ recursos/inicializar-postgreSQL.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-
-[ -z $PGUSER ] && PGUSER="postgres"
-[ -z $PGPASSWORD ] && PGPASSWORD="postgres"
+echo "Considere cambiar recursos/inicializar-postgreSQL.sh para que tenga un usuario y contraseña apropiados para usar postgres con permisos de administrador."
+[ -z $PGUSER ] && PGUSER="postgres" # cambiar con usuario de admin de postgres
+[ -z $PGPASSWORD ] && PGPASSWORD="postgres" # cambiar con contraseña apropiada
 [ -z $PGDATABASE ] && PGDATABASE="terox_db"
 [ -z $PGHOST ] && PGHOST=localhost
 [ -z $PGPORT ] && PGPORT=5432
-
-function die() {
-	echo "Necesitás tener las variables de ambiente listas para administrar postgreSQL e inicializar la base de datos"
-	echo "Un ejemplo es usar \`export PGUSER=postgres PGPASSWORD=postgres PGPORT=5432\`."
-}
-die
 
 # Inicializamos la base de datos
 psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -a -f creacion-db.sql || exit
 psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -a -f creacion-schema.sql || exit
 psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -a -f creacion-funciones.sql || exit
 psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -a -f dar-permisos.sql || exit
+cd recursos
 [ ! -e 'local.env' ] && cp 'ejemplo-local.env' 'local.env' # no existe un script con las variables de ambiente deseadas, así que copiamos el template
 # cambiamos las variables de ambiente para trabajar con la base de datos sin privilegios máximos
 source 'local.env'
+cd ..
 psql -U $DB_USER -d $DB_DATABASE -h $DB_HOST -p $DB_PORT -a -f cargar-datos-de-test.sql
