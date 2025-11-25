be9bf547337aa8638c21bbf4ef9551261d0aa8a1
joalozano <joaquin.lozano.trabajo@gmail.com>
2025-11-25 15:40:12 -0300

    Agregando listeners al back para generar facturas cuando se paga una orden

--- src/config/listener.ts
+++ src/config/listener.ts
@@ -1,5 +1,6 @@
 import { listener } from "./db";
 import { borrarImagen } from "../services/image-services";
+import { executeQuery } from "../services/queryExecutor";
 import { Notification } from "pg";
 
 export async function initListener() {
@@ -11,8 +12,23 @@ export async function initListener() {
       if (!link) console.error("[ERROR] Payload vacío en imagen_borrada");
       await borrarImagen(link as string);
     }
+	if (msg.channel === "orden_aceptada") {
+	  const orden_id: int = msg.payload;
+	  await crearFactura(orden_id as int);
+	}
   });
 
   await listener.query("LISTEN imagen_borrada");
   console.log("Listener Activo, esperando eventos...");
+}
+
+async function crearFactura(orden_id: int) {
+	const query_ObtenerUsernames = `SELECT comprador_username, vendedor_username FROM terox.ordenes WHERE orden_id = $1`;
+	const query_ObtenerCUIL = `SELECT cuil FROM terox.identidad_fiscal WHERE username = $1`;
+	const query_crearFactura = `INSERT INTO terox.facturas VALUES($1, $2, $3)`;
+	usernames = await executeQuery(query_ObtenerUsernames, [orden_id], '');
+	cuil_comprador = await executeQuery(query_ObtenerCUIL, [usernames.rows[0]?.comprador_username],'');
+	cuil_vendedor = await executeQuery(query_ObtenerCUIL, [usernames.rows[0]?.vendedor_username],'');
+	await executeQuery(query_crearFactura, [orden_id, cuil_comprador, cuil_vendedor], 'Error al crear factura');
+	return;
 }
