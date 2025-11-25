8b2a6903c039a1485f6a6d2578795e5e5721768a
joalozano <joaquin.lozano.trabajo@gmail.com>
2025-11-25 20:51:22 -0300

    Arreglando función de eliminación de usuario, donde se usan los atributos correctos al actualizar

--- recursos/dar-permisos.sql
+++ recursos/dar-permisos.sql
@@ -7,10 +7,10 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON terox.usuarios TO terox_admin;
 GRANT SELECT, INSERT, UPDATE, DELETE ON terox.productos TO terox_admin;
 GRANT USAGE, SELECT, UPDATE ON SEQUENCE terox.productos_producto_id_seq TO terox_admin;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON terox.ordenes TO terox_admin;
+GRANT SELECT, INSERT, UPDATE ON terox.ordenes TO terox_admin;
 GRANT USAGE, SELECT, UPDATE ON SEQUENCE terox.ordenes_orden_id_seq TO terox_admin;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON terox.facturas TO terox_admin;
+GRANT SELECT, INSERT, UPDATE ON terox.facturas TO terox_admin;
 GRANT USAGE, SELECT, UPDATE ON SEQUENCE terox.facturas_factura_id_seq TO terox_admin;
 
 GRANT SELECT, INSERT, UPDATE ON terox.identidad_fiscal TO terox_admin;
