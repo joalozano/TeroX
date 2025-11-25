ab85cb458c822482488b6156910720bd08567874
joalozano <joaquin.lozano.trabajo@gmail.com>
2025-11-25 18:05:48 -0300

    Agregando archivos para inicializar la app en windows (posiblemente, no se puede testear)

--- recursos/inicializar-postgreSQL.sh
+++ recursos/inicializar-postgreSQL.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Considere cambiar recursos/inicializar-postgreSQL.sh para que tenga un usuario y contraseña apropiados para usar postgres con permisos de administrador."
+
 [ -z $PGUSER ] && PGUSER="postgres" # cambiar con usuario de admin de postgres
 [ -z $PGPASSWORD ] && PGPASSWORD="postgres" # cambiar con contraseña apropiada
 [ -z $PGDATABASE ] && PGDATABASE="terox_db"
@@ -7,13 +8,15 @@ echo "Considere cambiar recursos/inicializar-postgreSQL.sh para que tenga un usu
 [ -z $PGPORT ] && PGPORT=5432
 
 # Inicializamos la base de datos
+cd recursos
 psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -a -f creacion-db.sql || exit
 psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -a -f creacion-schema.sql || exit
 psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -a -f creacion-funciones.sql || exit
 psql -U $PGUSER -d $PGDATABASE -h $PGHOST -p $PGPORT -a -f dar-permisos.sql || exit
-cd recursos
-[ ! -e 'local.env' ] && cp 'ejemplo-local.env' 'local.env' # no existe un script con las variables de ambiente deseadas, así que copiamos el template
+
 # cambiamos las variables de ambiente para trabajar con la base de datos sin privilegios máximos
+[ ! -e 'local.env' ] && cp 'ejemplo-local.env' 'local.env' # no existe un script con las variables de ambiente deseadas, así que copiamos el template
 source 'local.env'
-cd ..
+
 psql -U $DB_USER -d $DB_DATABASE -h $DB_HOST -p $DB_PORT -a -f cargar-datos-de-test.sql
+cd ..
