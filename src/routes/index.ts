dc5fc2cdc3e3e5ca649887bbbe33189846823fd8
joalozano <joaquin.lozano.trabajo@gmail.com>
2025-11-25 20:33:30 -0300

    Agregando creacion dinamica de tablas en js

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,20 +8,30 @@ import imagenes_routes from "./images-routes";
 import { requireAuthAPI, replacePasswordForHash, cantChangePassword } from "../middlewares/middlewares-auth";
 import { verificar_usuario_es_dueño_del_producto, añadir_username_a_request } from "../middlewares/middlewares-productos";
 import { requiere_usuario_es_dueño_de_identidad_fiscal } from "../middlewares/middlewares-id-fiscal";
-//import { executeQuery } from "../services/queryExecutor";
+import { executeQuery } from "../services/queryExecutor";
 import { HttpError } from "../types/http-error";
 import { FiltroSimple } from "../types/queryfilters";
 
 const router = Router();
 
-/*async function obtener_nombresTablas(tabla: string) {
+async function obtenerNombresTablas(tabla: string) {
 	const query_metadata = "SELECT column_name FROM information.columns WHERE table_schema='terox' AND table_name=$1";
 	const res = await executeQuery(query_metadata, [tabla], '');
-	return res.rows.map(r => r.column_name);
-}*/
+	const nombres = res.rows.map(r => r.column_name);
+	return nombres;
+}
 
-const atributos_producto = ["producto_id", "nombre", "descripcion", "precio", "stock", "username"];
-//const atributos_producto = await obtener_nombresTablas("productos");
+let atributos_producto!: string[];
+let atributos_usuario!: string[];
+let atributos_identidad_fiscal!: string[];
+
+(async() => {
+	atributos_producto = await obtenerNombresTablas("productos");
+	atributos_usuario = await obtenerNombresTablas("usuarios");
+	atributos_identidad_fiscal = await obtenerNombresTablas("identidad_fiscal");
+});
+
+//const atributos_producto = ["producto_id", "nombre", "descripcion", "precio", "stock", "username"];
 const middlewares_producto: MiddlewareCRUD = {
     get: [],
     post: [requireAuthAPI, añadir_username_a_request],
@@ -37,8 +47,7 @@ const query_params_get_producto = [
 router.use("/api", generarCRUD("/productos", "producto_id", atributos_producto, middlewares_producto, query_params_get_producto, true));
 router.use("/api", productos_routes);
 
-const atributos_usuario = ["username", "password_hash", "nombre", "email"];
-//const atributos_usuario = await obtener_nombresTablas("usuarios");
+//const atributos_usuario = ["username", "password_hash", "nombre", "email"];
 const middlewares_usuarios: MiddlewareCRUD = {
     get: [(_, res, __) => { res.sendStatus(403); }],
     post: [replacePasswordForHash],
@@ -48,9 +57,7 @@ const middlewares_usuarios: MiddlewareCRUD = {
 
 router.use("/api", generarCRUD("/usuarios", "username", atributos_usuario, middlewares_usuarios, [], false));
 
-
-const atributos_identidad_fiscal = ["cuil", "nombre_completo", "domicilio_fiscal"];
-//const atributos_identidad_fiscal = await obtener_nombresTablas("identidad_fiscal");
+//const atributos_identidad_fiscal = ["cuil", "nombre_completo", "domicilio_fiscal"];
 const middlewares_identidad_fiscal: MiddlewareCRUD = {
     get: [requireAuthAPI, requiere_usuario_es_dueño_de_identidad_fiscal],
     post: [requireAuthAPI],
