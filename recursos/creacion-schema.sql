12321ec5f766e501009c2ba9b24756850b1b8427
joalozano <joaquin.lozano.trabajo@gmail.com>
2025-11-25 21:12:49 -0300

    Cambiando el schema en base a un nuevo modelo para facturas

--- recursos/creacion-schema.sql
+++ recursos/creacion-schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS terox.usuarios (
 -- Tabla de Identidad Fiscal
 CREATE TABLE terox.identidad_fiscal (
 	cuil INT PRIMARY KEY,
-	username TEXT NOT NULL REFERENCES terox.usuarios(username),
+	username TEXT UNIQUE NOT NULL REFERENCES terox.usuarios(username),
 	nombre_completo TEXT NOT NULL,
 	domicilio_fiscal TEXT NOT NULL
 );
@@ -65,5 +65,9 @@ CREATE TABLE terox.facturas (
     factura_id SERIAL PRIMARY KEY,
     orden_id BIGINT NOT NULL REFERENCES terox.ordenes(orden_id), -- esta podria ser la primary key tambien
     comprador_identidad_fiscal_id BIGINT NOT NULL REFERENCES terox.identidad_fiscal(cuil),
+	comprador_nombre_completo TEXT NOT NULL,
+	comprador_domicilio_fiscal TEXT NOT NULL,
     vendedor_identidad_fiscal_id BIGINT NOT NULL REFERENCES terox.identidad_fiscal(cuil)
+	vendedor_nombre_completo TEXT NOT NULL,
+	vendedor_domicilio_fiscal TEXT NOT NULL
 );
