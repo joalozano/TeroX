bd0f5478390a41fa0a9c02de8858f6377bfc6002
Nacho <lucasignaciorodriguez123compu@gmail.com>
2025-11-26 19:55:15 -0300

    Se añade middleware para productos, debe de haber id fiscal antes de poder agregar producto

--- src/scripts/producto/agregar-producto.ts
+++ src/scripts/producto/agregar-producto.ts
@@ -24,7 +24,8 @@ async function agregarProducto(urlProducto: string, urlImagen: string) {
         });
 
         if (!resp.ok) {
-            mostrarNotificacion("No se pudo agregar producto", "error");
+            const error = await resp.json();
+            mostrarNotificacion(error.error, "error");
             return;
         }
 
