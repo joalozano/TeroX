e7a6d7040189c947a5776cd1f427ba70f710ec70
cozin101 <joalozano@proton.me>
2025-11-27 15:07:16 -0300

    Eliminando funciones creadas al invocar 'borrar-schema'; arreglando scripts de inicializacion

--- recursos/borrar-schema.sql
+++ recursos/borrar-schema.sql
@@ -1,2 +1,6 @@
 -- borrar esquema fácilmente para luego correr inicializarPostgreSQL.sh
 DROP SCHEMA terox CASCADE;
+-- borrar funciones y triggers creados, con cuidado
+DROP FUNCTION notificar_imagen_borrada CASCADE;
+DROP FUNCTION procesar_producto_usuario_borrado CASCADE;
+DROP FUNCTION registrar_pago CASCADE;
