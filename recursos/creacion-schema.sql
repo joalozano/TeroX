3b678a98d53b4a777d8ceb60f19b3a8138b56e13
cozin101 <joalozano@proton.me>
2025-11-27 13:36:47 -0300

    Agregando arreglos. Ahora los triggers y funciones andan (en psql)

--- recursos/creacion-schema.sql
+++ recursos/creacion-schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS terox.usuarios (
 -- Tabla de Identidad Fiscal
 CREATE TABLE terox.identidad_fiscal (
 	cuil BIGINT PRIMARY KEY,
-	username TEXT UNIQUE REFERENCES terox.usuarios(username) ON DELETE SET NULL,
+	username TEXT UNIQUE REFERENCES terox.usuarios(username) ON DELETE SET NULL ON UPDATE CASCADE,
 	nombre_completo TEXT NOT NULL,
 	domicilio_fiscal TEXT NOT NULL
 );
@@ -23,8 +23,8 @@ CREATE TABLE terox.identidad_fiscal (
 CREATE TABLE IF NOT EXISTS terox.productos (
 	producto_id SERIAL PRIMARY KEY,
 	nombre VARCHAR(100) NOT NULL,
-	precio INT NOT NULL CHECK (precio >= 0),
-	stock INT NOT NULL CHECK (stock >= 0),
+	precio INT CHECK (precio > 0),
+	stock INT CHECK (stock >= 0),
 	descripcion TEXT,
 	username TEXT REFERENCES terox.usuarios(username) ON UPDATE CASCADE ON DELETE SET NULL,
 	rating INT CHECK (rating >= 0 AND rating <= 5) DEFAULT 0,
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS terox.productos (
 -- url es relativa a alguna carpeta base de imágenes
 CREATE TABLE IF NOT EXISTS terox.imagenes(
 	imagen_id SERIAL PRIMARY KEY,
-	producto_id INT NOT NULL REFERENCES terox.productos(producto_id) ON DELETE CASCADE,
+	producto_id INT REFERENCES terox.productos(producto_id) ON DELETE CASCADE,
 	url TEXT NOT NULL
 );
 
@@ -44,15 +44,15 @@ CREATE TABLE IF NOT EXISTS terox.imagenes(
 CREATE TABLE IF NOT EXISTS terox.ordenes (
     orden_id SERIAL PRIMARY KEY,
 
-    producto_id INT NOT NULL REFERENCES terox.productos(producto_id),
+    producto_id SERIAL REFERENCES terox.productos(producto_id),
     producto_nombre TEXT NOT NULL,
-    comprador_username TEXT REFERENCES terox.usuarios(username) ON DELETE SET NULL,
-    vendedor_username TEXT REFERENCES terox.usuarios(username) ON DELETE SET NULL,
+    comprador_username TEXT REFERENCES terox.usuarios(username) ON DELETE SET NULL ON UPDATE CASCADE,
+    vendedor_username TEXT REFERENCES terox.usuarios(username) ON DELETE SET NULL ON UPDATE CASCADE,
 
     direccion_entrega TEXT NOT NULL,
 
-    cantidad_pedida INT NOT NULL CHECK (cantidad_pedida > 0),
-    precio_unitario INT NOT NULL CHECK (precio_unitario >= 0),
+    cantidad_pedida INT CHECK (cantidad_pedida > 0),
+    precio_unitario INT CHECK (precio_unitario > 0),
 
     estado_de_entrega VARCHAR(32) NOT NULL CHECK (
     estado_de_entrega IN ('esperando_producto_vendedor',
@@ -67,11 +67,11 @@ CREATE TABLE IF NOT EXISTS terox.ordenes (
 -- Tabla de Facturas
 CREATE TABLE IF NOT EXISTS terox.facturas (
     factura_id SERIAL PRIMARY KEY,
-    orden_id INT NOT NULL REFERENCES terox.ordenes(orden_id),
-    comprador_identidad_fiscal_id BIGINT NOT NULL REFERENCES terox.identidad_fiscal(cuil),
+    orden_id SERIAL REFERENCES terox.ordenes(orden_id),
+    comprador_identidad_fiscal_id BIGINT REFERENCES terox.identidad_fiscal(cuil) ON UPDATE CASCADE,
     comprador_nombre_completo TEXT NOT NULL,
     comprador_domicilio_fiscal TEXT NOT NULL,
-    vendedor_identidad_fiscal_id BIGINT NOT NULL REFERENCES terox.identidad_fiscal(cuil),
+    vendedor_identidad_fiscal_id BIGINT REFERENCES terox.identidad_fiscal(cuil) ON UPDATE CASCADE,
     vendedor_nombre_completo TEXT NOT NULL,
     vendedor_domicilio_fiscal TEXT NOT NULL
 );
@@ -79,8 +79,9 @@ CREATE TABLE IF NOT EXISTS terox.facturas (
 -- Tabla de Pagos a vendedores
 CREATE TABLE IF NOT EXISTS terox.pagos (
 	pago_id SERIAL PRIMARY KEY,
-	cuil BIGINT NOT NULL REFERENCES terox.identidad_fiscal(cuil),
-	vendedor_nombre_completo TEXT NOT NULL,
-	vendedor_domicilio_fiscal TEXT NOT NULL,
-	monto INT CHECK (monto >= 0)
+	orden_id SERIAL REFERENCES terox.ordenes(orden_id),
+	cuil BIGINT REFERENCES terox.identidad_fiscal(cuil) ON UPDATE CASCADE,
+	nombre_completo TEXT NOT NULL,
+	domicilio_fiscal TEXT NOT NULL,
+	monto INT CHECK (monto > 0)
 );
